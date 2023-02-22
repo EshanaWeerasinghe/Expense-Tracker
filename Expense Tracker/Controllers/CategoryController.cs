b5034521719cd76f5dba9506d28960c0d5637d60
Eshana Milan <eshana.m@evicio.com>
2023-02-22 15:05:15 +0530

    #AddOrEdit form

--- Expense Tracker/Controllers/CategoryController.cs
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -42,10 +42,12 @@ namespace Expense_Tracker.Controllers
             return View(category);
         }
 
-        // GET: Category/Create
-        public IActionResult Create()
-        {
+        // GET: Category/AddOrEdit
+        public IActionResult AddOrEdit(int id=0)
+        {   if(id==0)
             return View(new Category());
+        else
+                return View(_context.Categories.Find(id));
         }
 
         // POST: Category/Create
@@ -53,7 +55,7 @@ namespace Expense_Tracker.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CategoryID,Title,Icon,Type")] Category category)
+        public async Task<IActionResult> AddOrEdit([Bind("CategoryID,Title,Icon,Type")] Category category)
         {
             if (ModelState.IsValid)
             {
@@ -63,58 +65,7 @@ namespace Expense_Tracker.Controllers
             }
             return View(category);
         }
-
-        // GET: Category/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories.FindAsync(id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-            return View(category);
-        }
-
-        // POST: Category/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CategoryID,Title,Icon,Type")] Category category)
-        {
-            if (id != category.CategoryID)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(category);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoryExists(category.CategoryID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(category);
-        }
-
+     
         // GET: Category/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
